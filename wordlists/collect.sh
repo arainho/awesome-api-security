d52c11ee40df7d11def303b34e180b6bd9105c66
André Rainho <arainho.it@gmail.com>
2021-10-26 22:16:47 +0100

    create wordlists collect script

---
+++ wordlists/collect.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+curl -o common-api-endpoints-mazen160.txt https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/common-api-endpoints-mazen160.txt
+curl -o yassineaboukir-3203-common-api-endpoints.txt https://gist.githubusercontent.com/yassineaboukir/8e12adefbd505ef704674ad6ad48743d/raw/3ea2b7175f2fcf8e6de835c72cb2b2048f73f847/List%2520of%2520API%2520endpoints%2520&%2520objects
+curl -o danielmiessler-SecLists-swagger.txt https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/swagger.txt
+curl -o danielmiessler-SecLists-api-endpoints.txt https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/api/api-endpoints.txt
+curl -o danielmiessler-SecLists-graphql.txt https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/graphql.txt
+curl -o kiterunner-swagger-wordlist.txt https://wordlists-cdn.assetnote.io/data/kiterunner/swagger-wordlist.txt
+curl -o httparchive_apiroutes_2021_08_28.txt https://wordlists-cdn.assetnote.io/./data/automated/httparchive_apiroutes_2021_08_28.txt
