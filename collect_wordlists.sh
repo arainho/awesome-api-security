16197ddee7022b79dcc915c7af402790df797615
André Rainho <arainho.it@gmail.com>
2021-10-28 11:25:06 +0100

    remove old files

--- collect_wordlists.sh
+++ collect_wordlists.sh
@@ -16,6 +16,10 @@ curl -o "${TARGET_DIR}"/danielmiessler-SecLists-graphql.txt "https://raw.githubu
 curl -o "${TARGET_DIR}"/kiterunner-swagger-wordlist.txt "https://wordlists-cdn.assetnote.io/data/kiterunner/swagger-wordlist.txt"
 curl -o "${TARGET_DIR}"/httparchive_apiroutes_2021_08_28.txt "https://wordlists-cdn.assetnote.io/./data/automated/httparchive_apiroutes_2021_08_28.txt"
 
+# remove old files
+rm -- "./*.txt"
+rm -- "${TARGET_DIR}/*.txt"
+
 # create new bundle
 cat -- "${TMP_DIR}/*.txt"  > "${TMP_BUNDLE}"
 cat -- "${TMP_BUNDLE}" | sort -n | uniq > "${TARGET_DIR}/wordlist_bundle_$(date +"%Y-%m-%d").txt"
