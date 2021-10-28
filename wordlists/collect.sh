80d1df0fa1f5cd9312f7a3df6e092f138073c65b
André Rainho <arainho.it@gmail.com>
2021-10-28 10:57:58 +0100

    remove current wordlists

--- wordlists/collect.sh
+++ wordlists/collect.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TARGET_DIR="${1:-wordlists}"
+TARGET_DIR="${1:-wordlists/tmp/}"
 
 curl -o "${TARGET_DIR}"/common-api-endpoints-mazen160.txt "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/common-api-endpoints-mazen160.txt"
 curl -o "${TARGET_DIR}"/yassineaboukir-3203-common-api-endpoints.txt "https://gist.githubusercontent.com/yassineaboukir/8e12adefbd505ef704674ad6ad48743d/raw/3ea2b7175f2fcf8e6de835c72cb2b2048f73f847/List%2520of%2520API%2520endpoints%2520&%2520objects"
@@ -9,3 +9,8 @@ curl -o "${TARGET_DIR}"/danielmiessler-SecLists-api-endpoints.txt "https://raw.g
 curl -o "${TARGET_DIR}"/danielmiessler-SecLists-graphql.txt "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/graphql.txt"
 curl -o "${TARGET_DIR}"/kiterunner-swagger-wordlist.txt "https://wordlists-cdn.assetnote.io/data/kiterunner/swagger-wordlist.txt"
 curl -o "${TARGET_DIR}"/httparchive_apiroutes_2021_08_28.txt "https://wordlists-cdn.assetnote.io/./data/automated/httparchive_apiroutes_2021_08_28.txt"
+
+rm -- "*.txt"
+#cat -- "*.txt"  > tmp_bunble.txt
+#cat -- tmp_bunble.txt | sort -n | uniq > wordlist_bundle_$(date +"%Y-%m-%d").txt
+#rm -fv -- "tmp_bunble.txt"
