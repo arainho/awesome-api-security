ff063829d0bf85d7665decb0ac7cb56c141a1253
André Rainho <arainho.it@gmail.com>
2021-11-16 19:07:17 +0000

    Create damn-vulnerable-microservices.sh

---
+++ labs/damn-vulnerable-microservices.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(<kubectl.sha256) kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+# skafold
+curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && \
+sudo install skaffold /usr/local/bin/
+
+# DamnVulnerableMicroServices
+git clone --depth=1 git@github.com:ne0z/DamnVulnerableMicroServices.git
+git submodule update --init
+skaffold dev
